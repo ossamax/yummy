a9d5ce20f4cde49b99f8339f4f393dbdda6ad755
ossama zaoute <ramziioussama@gmail.com>
2021-05-17 17:04:51 +0100

    pushing to the respo

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const yummyMenu = document.querySelector(".dots_menu_icon");
+const closeIcon = document.querySelector(".right_close_img");
+const dotsIcon = document.querySelector(".dots_img");
+
+const yummyWrapper = document.querySelector(".yummy_wrapper");
+
+yummyMenu.addEventListener("click", () => {
+  if (yummyWrapper.classList.contains("active")) {
+    yummyWrapper.classList.remove("active");
+    closeIcon.classList.remove("active");
+    dotsIcon.classList.remove("disactive");
+  } else {
+    yummyWrapper.classList.add("active");
+    closeIcon.classList.add("active");
+    dotsIcon.classList.add("disactive");
+
+    main_nav_links.classList.remove("active");
+    closeIconLeft.classList.remove("active");
+    barsIcon.classList.remove("disactive");
+  }
+});
+
+//main navigation icons vars
+const burgerMenuIcon = document.querySelector(".burger_menu_icon");
+const closeIconLeft = document.querySelector(".left_close_img");
+const barsIcon = document.querySelector(".bars_img");
+const main_nav_links = document.querySelector(".main_nav_links");
+
+burgerMenuIcon.addEventListener("click", () => {
+  if (main_nav_links.classList.contains("active")) {
+    main_nav_links.classList.remove("active");
+    closeIconLeft.classList.remove("active");
+    barsIcon.classList.remove("disactive");
+  } else {
+    main_nav_links.classList.add("active");
+    closeIconLeft.classList.add("active");
+    barsIcon.classList.add("disactive");
+
+    yummyWrapper.classList.remove("active");
+    closeIcon.classList.remove("active");
+    dotsIcon.classList.remove("disactive");
+  }
+});
+
+//remove nav classes
+
+function hideNav() {
+  yummyWrapper.classList.remove("active");
+  closeIcon.classList.remove("active");
+  dotsIcon.classList.remove("disactive");
+  //
+  main_nav_links.classList.remove("active");
+  closeIconLeft.classList.remove("active");
+  barsIcon.classList.remove("disactive");
+}
