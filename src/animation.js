ac4148b1c08c37f5be742c50b2fa2d535afec8fd
ossama zaoute <ramziioussama@gmail.com>
2021-05-25 13:38:17 +0100

    update

--- src/animation.js
+++ src/animation.js
@@ -37,3 +37,40 @@ gsap.to(".house_dishes", {
     trigger: ".house_dishes",
   },
 });
+
+gsap.to(".testemonial", {
+  opacity: 1,
+  y: 0,
+  duration: 0.6,
+  scrollTrigger: {
+    trigger: ".testemonial",
+  },
+});
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".event_name",
+      start: "bottom bottom",
+      end: "bottom bottom",
+      scrub: true,
+    },
+  })
+  .to(".event_name", { rotateZ: 360, opacity: 1 }, 0);
+
+gsap.to(".map_wrapper", {
+  opacity: 1,
+  x: 0,
+  duration: 0.7,
+  scrollTrigger: {
+    trigger: ".map_wrapper",
+  },
+});
+gsap.to(".contact_form", {
+  opacity: 1,
+  x: 0,
+  duration: .7,
+  scrollTrigger: {
+    trigger: ".contact_form",
+  },
+});
